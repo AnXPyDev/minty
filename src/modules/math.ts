f0b3405ed9dfc09dea1df8984b3d3924c36604d6
Unknown <jkism332@gmail.com>
2018-04-12 14:26:03 +0200

    started angle class

--- src/modules/math.ts
+++ src/modules/math.ts
@@ -13,6 +13,39 @@ class Vector {
     }
 }
 
+class Angle {
+    public rad:number;
+    public deg:number;
+    private default:string;
+    private types:string[];
+
+    public to(value:number, type:string) {}
+
+    constructor(defaulttype:string = "rad", value:number) {
+        this.types = ["deg", "rad"];
+        this.default = "rad";
+        if (defaulttype == this.types[0] || defaulttype == this.types[1]) {
+            this.default = defaulttype;
+        }
+        this.deg = function(type:string):number {
+            if (type == "rad") {
+                return value  * 180 / Math.PI;
+            }
+            return value;
+        }(this.default);
+        this.rad = function(type:string):number {
+            if (type == "deg") {
+                return value * Math.PI / 180;
+            }
+            return value;
+        }(this.default);
+    }
+
+    set(value:number, type:string = this.default) {
+
+    }
+}
+
 module.exports = {
     Vector:Vector
 }
