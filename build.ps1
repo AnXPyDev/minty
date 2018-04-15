6b96a7be1c35831142d8870cca19bc22afa76e69
Unknown <jkism332@gmail.com>
2018-04-15 07:15:33 +0200

    removed build.ps1

--- build.ps1
+++
@@ -1,4 +0,0 @@
-tsc;
-node buildConfig.js;
-node buildGame.js;
-./node_modules/electron/dist/electron.exe .;
--- buildGame.js
+++ buildGame.js
@@ -5,32 +5,55 @@
         image: ["jpg", "svg", "png"],
         sound: ["mp3"]
     }
+    let ec = 0;
     fs.readdir("./project/code", function (err, files) {
-        files.forEach(file => {
-            let extension = file.split(".")[1];
-            if (extension == "js") {
-                json.code.js.push(file);
-            } else if (extension == "json") {
-                json.code.json.push(file);
-            }
-        });
-        fs.readdir("./project/assets/img", function (err, files) {
+        if (err) {
+            console.log("The folder 'project/code' is missing");
+            ec++;
+        } else {
             files.forEach(file => {
                 let extension = file.split(".")[1];
-                if (types.image.includes(extension)) {
-                    json.assets.images.push(file);
+                if (extension == "js") {
+                    json.code.js.push(file);
+                } else if (extension == "json") {
+                    json.code.json.push(file);
                 }
-            })
-            fs.readdir("./project/assets/snd", function (err, files) {
+            });
+        }
+        fs.readdir("./project/assets/img", function (err, files) {
+            if (err) {
+                console.log("The folder 'project/assets/img' is missing");
+                ec++;
+            } else {
                 files.forEach(file => {
                     let extension = file.split(".")[1];
-                    if (types.sound.includes(extension)) {
-                        json.assets.sounds.push(file);
+                    if (types.image.includes(extension)) {
+                        json.assets.images.push(file);
                     }
-                });
-                fs.writeFile("./project/game.cfg.json", JSON.stringify(json), function() {
-                    console.log("Sucessfully built game.cfg.json");
-                });
+                })
+            }
+            fs.readdir("./project/assets/snd", function (err, files) {
+                if (err) {
+                    console.log("The folder 'project/assets/snd' is missing")
+                } else {
+                    files.forEach(file => {
+                        let extension = file.split(".")[1];
+                        if (types.sound.includes(extension)) {
+                            json.assets.sounds.push(file);
+                        }
+                    });
+                }
+                if (!ec) {
+                    fs.writeFile("./project/game.cfg.json", JSON.stringify(json), function() {
+                        console.log("Sucessfully built game.cfg.json");
+                    });
+                } else {
+                    console.log("Multiple errors have occured, please make sure there is 'project' folder with this structure in this folder !");
+                    console.log("project/code - folder");
+                    console.log("project/assets - folder");
+                    console.log("project/assets/img - subfolder");
+                    console.log("project/assets/snd - subfolder");
+                }
             })
         })
     });
