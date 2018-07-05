4c32c84693798fbc3343e128b87766d510775c7f
Unknown <jkism332@gmail.com>
2018-07-05 17:28:10 +0200

    added offset to backgrounds

--- src/modules/background.ts
+++ src/modules/background.ts
@@ -8,6 +8,8 @@ class Background {
     public scale:Vector;
     public off:Vector;
     public spd:Vector;
+    public offset:Vector;
+    public aoff:Vector;
     private types:string[];
 
     constructor(imgname:string[], type:string, color:string = "white") {
@@ -28,14 +30,16 @@ class Background {
         this.spd = new Vector();
         this.depth = -15;
         this.alpha = 1;
+        this.offset = v();
+        this.aoff = v();
     }
     draw():void {
         ctx.save();
         ctx.globalAlpha = this.alpha;
         if(this.type == "tiled") {
             let goff:Vector = new Vector(
-                    (camera.pos.x / (this.img.get().width * this.scale.x) - Math.floor(camera.pos.x / (this.img.get().width * this.scale.x))) * (this.img.get().width * this.scale.x),
-                    (camera.pos.y / (this.img.get().height * this.scale.y) - Math.floor(camera.pos.y / (this.img.get().height * this.scale.y))) * (this.img.get().height * this.scale.y),
+                    (camera.pos.x / (this.img.get().width * this.scale.x) - Math.floor(camera.pos.x / (this.img.get().width * this.scale.x))) * (this.img.get().width * this.scale.x) + this.offset.x,
+                    (camera.pos.y / (this.img.get().height * this.scale.y) - Math.floor(camera.pos.y / (this.img.get().height * this.scale.y))) * (this.img.get().height * this.scale.y) + this.offset.y
                 )
             for(let i:number = -Math.floor((vport.size.x / 2) / (this.img.get().width * this.scale.x)) -2; i < Math.floor(vport.size.x / (this.img.get().width * this.scale.x)) + 2; i++) {
                 for(let e:number = -Math.floor((vport.size.y / 2) / (this.img.get().height * this.scale.y)) -2; e < Math.floor(vport.size.y / (this.img.get().height * this.scale.y)) + 2; e++) {
@@ -59,6 +63,8 @@ class Background {
     update():void {
         this.off.x = wrap(this.off.x + this.spd.x * dt, 0, this.img.get().width * this.scale.x);
         this.off.y = wrap(this.off.y + this.spd.y * dt, 0, this.img.get().height * this.scale.y);
+        this.offset.x = (this.aoff.x - Math.floor(this.aoff.x / this.img.get().width) * this.img.get().width) * this.scale.x;
+        this.offset.y = (this.aoff.y - Math.floor(this.aoff.y / this.img.get().height) * this.img.get().height) * this.scale.y;
         $MAIN.cLAY.insert(new Layer(this.depth, () => {return this.draw()}));
     }
     setScale(scale:Vector):void {
@@ -66,6 +72,9 @@ class Background {
     }
     setScroll(spd:Vector):void {
         this.spd = spd;
+    }
+    setOffset(off:Vector):void {
+        this.aoff = off;
     }
 }
 
