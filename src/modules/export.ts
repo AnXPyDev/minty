40a150389ad8e1155c2d98783434a02c036b912a
Unknown <jkism332@gmail.com>
2018-08-03 07:06:20 +0200

    saving progress in scene director

--- src/modules/export.ts
+++ src/modules/export.ts
@@ -3,6 +3,19 @@ function exportObjectAsJson(x:any, filename:string) {
     if (!fs.existsSync(`${paths.project}/exports`)){ 
         fs.mkdirSync(`${paths.project}/exports`); 
     } 
+    let split = filename.split("/");
+    let path = ""
+
+    if(split[0] != "..") {
+
+        for(let i = 0; i < split.length - 1; i++) {
+            path += split[i] + "/";
+            if(!fs.existsSync(`${paths.project}/exports/${path}`)) {
+                fs.mkdirSync(`${paths.project}/exports/${path}`);
+            }
+        }
+    }
+
     fs.writeFileSync(`${paths.project}/exports/${filename}.json`, json);   
 }
 
