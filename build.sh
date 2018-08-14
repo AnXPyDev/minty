e77cccde580ac4c17a4cf2a5466816dc3e9d12e1
Unknown <jkism332@gmail.com>
2018-08-14 07:04:42 +0200

    can build with developer mode

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-sh compile.sh $1 nodev;
+#!/bin/bash
+
+if [[ "$3" == "dev" ]];  then
+    sh compile.sh $1;
+else
+    sh compile.sh $1 nodev;
+fi
+
 echo "Building app ...";
 ./node_modules/.bin/electron-packager ./ $1 --out=../built --overwrite --platform=$2;
 cd scripts;
